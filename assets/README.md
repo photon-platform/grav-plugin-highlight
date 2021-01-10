50056026c8aa099e3c0d9421ca4e8b60e25aaba5
phiarchitect <phi@phiarchitect.com>
2021-01-10 14:30:56 -0800

    update highlight library

---
+++ assets/README.md
@@ -0,0 +1,3 @@
+download custom build with just the syntax sets we are likely to use
+
+https://highlightjs.org/download/
--- photon-highlight.php
+++ photon-highlight.php
@@ -6,56 +6,65 @@ use Grav\Common\Page\Interfaces\PageInterface;
 
 class PhotonHighlightPlugin extends Plugin
 {
-    /**
-     * @return array
-     */
-    public static function getSubscribedEvents()
-    {
-        return [
-            'onPageInitialized' => ['onPageInitialized', 0]
-        ];
+  /**
+   * @return array
+   */
+  public static function getSubscribedEvents()
+  {
+    return [
+      'onPageInitialized' => ['onPageInitialized', 0]
+    ];
+  }
+
+  /**
+   * Initialize configuration
+   */
+  public function onPageInitialized()
+  {
+    if ($this->isAdmin()) {
+      $this->active = false;
+      return;
     }
 
-    /**
-     * Initialize configuration
-     */
-    public function onPageInitialized()
-    {
-        if ($this->isAdmin()) {
-            $this->active = false;
-            return;
-        }
+    $defaults = (array) $this->config->get('plugins.photon-highlight');
 
-        $defaults = (array) $this->config->get('plugins.photon-highlight');
-
-        /** @var PageInterface $page */
-        $page = $this->grav['page'];
-        if (isset($page->header()->highlight)) {
-            $this->config->set('plugins.photon-highlight', array_merge($defaults, $page->header()->highlight));
-        }
-        if ($this->config->get('plugins.photon-highlight.enabled')) {
-            $this->enable([
-                'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
-            ]);
-        }
+    /** @var PageInterface $page */
+    $page = $this->grav['page'];
+    if (isset($page->header()->highlight)) {
+      // TODO: merge fails when page has 'highlight: true'
+      // $this->config->set('plugins.photon-highlight', array_merge($defaults, $page->header()->highlight));
     }
+    if ($this->config->get('plugins.photon-highlight.enabled')) {
+      $this->enable([
+        'onTwigSiteVariables' => ['onTwigSiteVariables', 0]
+      ]);
+    }
+  }
 
-    /**
-     * if enabled on this page, load the JS + CSS theme.
-     */
-    public function onTwigSiteVariables()
-    {
-        $init = "hljs.initHighlightingOnLoad();\n";
+  /**
+   * if enabled on this page, load the JS + CSS theme.
+   */
+  public function onTwigSiteVariables()
+  {
+    $page = $this->grav['page'];
+    $assets = $this->grav['assets'];
+
+    // only load the vars if this datatype page
+    $header = $page->header();
+    $highlight = isset($header->highlight) ? $header->highlight : false;
+
+    if ( $highlight ) {
+      $theme = $this->config->get('plugins.photon-highlight.theme') ?: 'default';
+      $assets->addCss('plugin://photon-highlight/assets/highlight-10.5.0/styles/'.$theme.'.css');
+      $assets->addJs('plugin://photon-highlight/assets/highlight-10.5.0/highlight.pack.js');
+
+      $init = "
+      hljs.initHighlightingOnLoad();\n";
         if ($this->config->get('plugins.photon-highlight.lines')) {
-            $init .= "hljs.initLineNumbersOnLoad();\n";
+          $init .= "
+      hljs.initLineNumbersOnLoad();\n";
         }
-        $theme = $this->config->get('plugins.photon-highlight.theme') ?: 'default';
-        $this->grav['assets']->addCss('plugin://photon-highlight/css/'.$theme.'.css');
-        $this->grav['assets']->addJs('plugin://photon-highlight/js/highlight.pack.js');
-        if ($this->config->get('plugins.photon-highlight.lines')) {
-            $this->grav['assets']->addJs('plugin://photon-highlight/js/highlightjs-line-numbers.min.js');
-            $this->grav['assets']->addCss('plugin://photon-highlight/css/highlightjs-line-numbers.css');
-        }
-        $this->grav['assets']->addInlineJs($init);
+      $assets->addInlineJs($init);
     }
+  }
 }
